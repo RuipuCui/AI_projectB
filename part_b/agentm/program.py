e56c59647ab7916d0802d120a45b2e7d0506eafc
ruipu cui <ruipucui@ruipus-MacBook-Air.local>
2024-05-07 16:46:42 +1000

    add alpha beta prunning

--- part_b/agentm/program.py
+++ part_b/agentm/program.py
@@ -88,11 +88,11 @@ class Agent:
                 #self.board.apply_action(action)
                 for p in possible_first:
                     first_actions.append(p.get_placeAction())
-                return choose_action(self.board, self._color, first_actions)
+                return chooseAction(self.board, self._color, first_actions)
         
         #action = random.choice(possible_actions)
         #self.board.apply_action(action)
-        return choose_action(self.board, self._color, possible_actions)
+        return chooseAction(self.board, self._color, possible_actions)
         
         
         
@@ -202,31 +202,38 @@ def heuristic(board: Board, color: PlayerColor, depth):
     #nprint(h)
     return h
 
-def chooseAction(actions, board: Board, color, length):
-    x = 0
-    maxh = 0
-    action = actions[0]
-    for a in actions:
+def chooseAction(board: Board, color, actions):
+    if len(actions) >= 50:
+        print("111112222233333")
+        maxh = 0
+        action = actions[0]
+        for a in actions:
         #print(x)
-        x += 1
+        #x += 1
         #if length < 10:
             #currh = heuristic_relax(board, color, a)
-        if length < 100:
-            board.apply_action(a) 
-            currh = heuristic(board, color, 4)
-            board.undo_action
-        elif 100 <= length:
+        #if length < 100:
+            #board.apply_action(a) 
+            #currh = heuristic(board, color, 4)
+            #board.undo_action
+        #elif 100 <= length:
+            #print(board.render(True, True))
             board.apply_action(a)
-            currh = heuristic(board, color, 3)
-            board.undo_action
+            currh = heuristic(board, color, 2)
+            board.undo_action()
+            #print(board.render(True, True))
         #else:
             #currh = heuristic(board, color, a, 4)
         
-        if currh > maxh:
-            maxh = currh
-            action = a
+            if currh > maxh:
+                maxh = currh
+                action = a
             #print(action, heuristic)
-    return action
+        return action
+    else:
+        print("aaaaaabbbbbbccccc")
+        action = choose_action(board, color, actions)
+        return action
 
 def checkEmpty(board):
     for key in board:
@@ -236,34 +243,43 @@ def checkEmpty(board):
     return True
 
 
+'''
+minmax(3 blur 1)
+expand(blue)
+get maximum h
+apply(a blue)
+    minmax(2 red 0)
+    expand(red)
+    get minimum h
+    apply(a, red)
+        minmax(1 blue 1)
+        expand(blue)
+        get maximum h
+        apply(a blue)
+            minmax(0 red 0)
+            return h(blue)
 
-def minMax(board: Board, depth, ismax, color: PlayerColor):
-    '''
-    noblue = 0
-    for key in board._state:
-        if board._state[key].player == PlayerColor.BLUE:
-            noblue = 1
-    if noblue == 0:
-        exit(0)
-    '''
 
-    print(board.render(True, True))
-    print("curr depth = ", depth, board._history[-1].action, color)
+
+
+'''
+def minMax(board: Board, depth, ismax, color: PlayerColor, alpha, beta):
+    #print(board.render(True, True))
+    #print("curr depth = ", depth, color)
 
     if depth == 0:
         #board.undo_action
         #print(board.render(True, True))
         if ismax == 0:
-            h = heuristic(board, color.opponent, 3)
+            h = heuristic(board, color.opponent, 4)
         else:
-            h = heuristic(board, color, 3)
+            h = heuristic(board, color, 4)
         board.undo_action()
         #print(board.render(True, True))
-        print("h = ", h)
+        #print("h = ", h)
         return h
     
     #board.apply_action(action)
-
     my_expension = get_all_expansion(board._state, color, 4)
     my_actions = []
     for e in my_expension:
@@ -287,23 +303,33 @@ def minMax(board: Board, depth, ismax, color: PlayerColor):
     if ismax == 1:
         currMax = float('-inf')
         for a in my_actions:
-            print("action = ", a, "color = ", color)
+            #print("action = ", a, "color = ", color)
             board.apply_action(a)
-            evaluation = minMax(board, depth - 1, ismax - 1, color.opponent)
+            evaluation = minMax(board, depth - 1, ismax - 1, color.opponent, alpha, beta)
             if evaluation > currMax:
                 currMax = evaluation
+
+            alpha = max(alpha, evaluation)
+            if beta <= alpha:
+                break  # Beta cutoff
+
         board.undo_action()
         return currMax
 
     else:
         currMin = float('inf')
         for a in my_actions:
-            print("action = ", a, "color = ", color)
+            #print("action = ", a, "color = ", color)
             board.apply_action(a)
             #print("action = ", a)
-            evaluation = minMax(board, depth - 1, ismax + 1, color.opponent)
+            evaluation = minMax(board, depth - 1, ismax + 1, color.opponent, alpha, beta)
             if evaluation < currMin:
                 currMin = evaluation
+
+            beta = min(beta, evaluation)
+            if beta <= alpha:
+                break  # Alpha cutoff
+
         board.undo_action()
         return currMin
 
@@ -311,23 +337,27 @@ def choose_action(board: Board, color: PlayerColor, my_actions):
 
     curr_best_move = my_actions[0]
     curr_best_value = float('-inf')
+
+    alpha = float('-inf')
+    beta = float('inf')
+
     for a in my_actions:
         #print("111")
         board.apply_action(a)
-        curr_value = minMax(board, 1, 0, color.opponent)
+        curr_value = minMax(board, 2, 0, color.opponent, alpha, beta)
         if curr_value > curr_best_value:
             curr_best_value = curr_value
             curr_best_move = a
+        alpha = max(alpha, curr_value)
+        if beta <= alpha:
+            break  # Beta cutoff
     #board.undo_action()
 
     return curr_best_move
 
 #python -m referee agentr.program agentm.program
+#python -m referee agenth.program agentm.program
     
-
-
-
-
 
 
 
